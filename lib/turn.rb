8517074165ebad74569551024726c16514cbfbba
Adam Clayman <adam@clayman.org>
2017-10-02 23:55:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,11 +39,9 @@ def turn(board)
     input = gets.strip
     index = input_to_index(input)
     if (valid_move?(board, index))
+      move(board, index)
+      display_board(board)
       break
     end
   end
-=begin
-  move(board, index)
-  display_board(board)
-=end
 end
